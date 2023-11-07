530858cdc805f936e9ef4ed3b832599f10f1df37
Katarzyna Zaloba <123471023+KatarzynaZaloba@users.noreply.github.com>
2023-11-07 17:41:21 +0100

    added Task to the Vue Controlled Components

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,15 +1,19 @@
 <template>
   <div :key="task.id" v-for="task in tasks">
-    <h3>{{ task.text }}</h3>
+    <Task :task="task" />
   </div>
 </template>
 
 <script>
+import Task from "./Task.vue";
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: "Tasks",
   props: {
     tasks: Array
+  },
+  components: {
+    Task,
   }
 }
 </script>
