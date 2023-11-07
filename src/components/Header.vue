59707f4db925ab17130366d2464d2eb0cb76d8ea
Katarzyna Zaloba <123471023+KatarzynaZaloba@users.noreply.github.com>
2023-11-07 17:15:31 +0100

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Header',
+  props: {
+    title: String,
+  }
+}
+</script>
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
